96361bdb4a0fa35650b0a4e3dd672e9f7dd171cd
AguzzDev <aribotta1901@gmail.com>
2024-10-05 09:13:00 -0300

    fix: dockerfile

--- server/src/modules/scraping/index.ts
+++ server/src/modules/scraping/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const scrapper = async () => {
   cron.schedule(
-    "10 9 * * *",
+    "15 9 * * *",
     async () => {
       try {
         const today = toZonedTime(new Date(), "America/Argentina/Buenos_Aires");
