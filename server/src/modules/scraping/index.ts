ffadcc9039335995f0f10a83510ccdcc0a777d9e
AguzzDev <aribotta1901@gmail.com>
2024-10-05 14:00:51 -0300

    chore: change time to scrape

--- server/src/modules/scraping/index.ts
+++ server/src/modules/scraping/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const scrapper = async () => {
   cron.schedule(
-    "46 13 * * *",
+    "5 14 * * *",
     async () => {
       try {
         const today = toZonedTime(new Date(), "America/Argentina/Buenos_Aires");
