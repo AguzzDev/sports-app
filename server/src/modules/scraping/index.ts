8c745d3b7910ddade55321515f4f9ab28b5d2b44
AguzzDev <aribotta1901@gmail.com>
2024-10-06 17:48:06 -0300

    test: remove cron

--- server/src/modules/scraping/index.ts
+++ server/src/modules/scraping/index.ts
@@ -10,48 +10,41 @@ import path from "path";
 dotenv.config();
 
 const scrapper = async () => {
-  cron.schedule(
-    "10 17 * * *",
-    async () => {
-      try {
-        const today = toZonedTime(new Date(), "America/Argentina/Buenos_Aires");
-        const browser = await puppeteer.launch({
-          headless: true,
-          timeout: 0,
-          executablePath: "chrome/linux-129.0.6668.58/chrome-linux64/chrome",
+  try {
+    const today = toZonedTime(new Date(), "America/Argentina/Buenos_Aires");
+    const browser = await puppeteer.launch({
+      headless: true,
+      timeout: 0,
+      executablePath: "chrome/linux-129.0.6668.58/chrome-linux64/chrome",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-zygote",
+        "--single-process",
+      ],
+    });
 
-          args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-zygote",
-            "--single-process",
-          ],
-        });
+    const page = await browser.newPage();
+    const page2 = await browser.newPage();
 
-        const page = await browser.newPage();
-        const page2 = await browser.newPage();
+    const date = format(today, "yyyy-MM-dd");
 
-        const date = format(today, "yyyy-MM-dd");
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 2);
 
-        const yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - 2);
+    await Match.deleteMany({
+      createdAt: { $lt: yesterday },
+    });
 
-        await Match.deleteMany({
-          createdAt: { $lt: yesterday },
-        });
+    allLeagueScrap({ page: page2 });
+    await dailyMatchesScrap({ page, date });
 
-        allLeagueScrap({ page: page2 });
-        await dailyMatchesScrap({ page, date });
-
-        await browser.close();
-      } catch (error) {
-        console.error("Error durante la tarea programada:", error);
-      }
-    },
-    { timezone: "America/Argentina/Buenos_Aires" }
-  );
+    await browser.close();
+  } catch (error) {
+    console.error("Error durante la tarea programada:", error);
+  }
 };
 
 export default scrapper;
